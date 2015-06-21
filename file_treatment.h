06c568c54c0abf404ec2306d3b6f260627688588
vilmabezerra <vilmabezerraalves@gmail.com>
2015-06-20 22:13:42 -0300

    Update file_treatment.h

--- file_treatment.h
+++ file_treatment.h
@@ -6,25 +6,29 @@
 std::vector<opt::transaction> file_input_treatment(std::string file){
 	std::ifstream inFile(file, std::ios::in | std::ios::binary);
 	char ch;
+	std::vector<opt::transaction> transaction_list;
 	std::string transaction_name;
 	std::string object_name;
-	std::vector<action> actions_sequence;
-	std::vector<string> read_objects, write_objects;
-	std::vector<transaction> transaction_list;
 
 	if(!inFile ) {
 	std::cout<< "Não foi possivel abrir o arquivo de entrada : "<< file<<"\n";
 	}else{
 		while (inFile.get(ch)){
+			std::vector<opt::action> actions_sequence;
+			std::vector<std::string> read_objects, write_objects;
+			transaction_name = "";
+
 			while(ch != ':'){
 				transaction_name += ch;			
 				inFile.get(ch);
 			}
 			while(ch != '\n'){
+				object_name = "";
 				if (ch == 'r'){
 					inFile.get(ch);
-					while((ch != '(') && (ch !=')')){
-						object_name += ch;
+					while(ch !=')'){
+						if (ch != '(')
+							object_name += ch;
 						inFile.get(ch);
 					}
 					read_objects.push_back(object_name);
@@ -32,8 +36,9 @@ std::vector<opt::transaction> file_input_treatment(std::string file){
 				}
 				if(ch == 'w'){
 					inFile.get(ch);
-					while((ch != '(') && (ch !=')')){
-						object_name += ch;
+					while(ch !=')'){
+						if (ch != '(')
+							object_name += ch;
 						inFile.get(ch);
 					}
 					write_objects.push_back(object_name);
